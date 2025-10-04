f61770fac46bbb3e1e68e0a99a7b5da3c9fd056a
Jamal Raja <Jamalraja083@gmail.com>
2025-10-04 18:15:38 +0100

    Implement to-do list functionality with item addition and user input validation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,27 @@
-const h1 = document.querySelector("h1");
+const list = document.getElementById("ul_id");
+const toDo = ["Brush hair", "Feed dog", "Buy Crippsies"];
 
-h1.innerText = "CHANGED! <-- != === >=";
+function refreshList() {
+  list.innerHTML = "";
+  for (item in toDo) {
+    const li = document.createElement("li");
+    li.innerHTML = toDo[item];
+    list.appendChild(li);
+  }
+}
+
+function addItem() {
+  const textArea = document.getElementById("text_area_id");
+  // Validate user input
+  if (textArea.value == "")
+    // If user did not enter anything, prompt user to enter a value
+    return window.alert("Please enter an item before pressing Add");
+  else if (textArea.value == toDo[0])
+    // If user enters a duplicate, display error
+    return window.alert("Error: You cannot have duplicate entries");
+
+  toDo.unshift(textArea.value);
+  refreshList();
+}
+
+refreshList();
