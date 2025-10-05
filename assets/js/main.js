e87991e2912a3933d3d6bda283c987433434906a
Jamal Raja <Jamalraja083@gmail.com>
2025-10-05 17:54:03 +0100

    Add edit and delete icons to to-do list items; update item validation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 const list = document.getElementById("ul_id");
-const toDo = ["Brush hair", "Feed dog", "Buy Crippsies"];
+const toDo = ["Brush hair", "Feed dog", "Buy Crippsies", "bath my horse"];
 
 /**
  * Render the current `toDo` array into the UL.
@@ -14,10 +14,16 @@ function refreshList() {
     li.id = i;
     list.appendChild(li);
 
-    // Delete control (bin icon)
+    // Action icons wrapper
     const span = document.createElement("span");
     span.innerHTML = `
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+      <svg class="edit-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+        <g fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+          <path d="M12 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
+          <path d="M18.375 2.625a1 1 0 0 1 3 3l-9.013 9.014a2 2 0 0 1-.853.505l-2.873.84a.5.5 0 0 1-.62-.62l.84-2.873a2 2 0 0 1 .506-.852z" />
+        </g>
+      </svg>
+      <svg class="bin-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
            viewBox="0 0 24 24" aria-hidden="true">
         <path d="m6.774 6.4l.812 13.648a.8.8 0 0 0 .798.752h7.232a.8.8 0 0 0 .798-.752L17.226 6.4h1.203l-.817 13.719A2 2 0 0 1 15.616 22H8.384a2 2 0 0 1-1.996-1.881L5.571 6.4zM9.5 9h1.2l.5 9H10zm3.8 0h1.2l-.5 9h-1.2zM4.459 2.353l15.757 2.778a.5.5 0 0 1 .406.58L20.5 6.4L3.758 3.448l.122-.69a.5.5 0 0 1 .579-.405m6.29-1.125l3.94.695a.5.5 0 0 1 .406.58l-.122.689l-4.924-.869l.122-.689a.5.5 0 0 1 .579-.406z"/>
       </svg>`;
@@ -34,7 +40,7 @@ refreshList();
 function addItem() {
   const textArea = document.getElementById("text_area_id");
 
-  if (textArea.value == "")
+  if (!textArea.value)
     return window.alert("Please enter an item before pressing Add");
   // =========== TODO: IMPROVE VALIDATION BY CHECKING ALL ITEMS IN LIST ===========
   else if (textArea.value == toDo[0])
@@ -46,12 +52,26 @@ function addItem() {
 }
 
 /**
- * Handle deletes: clicking the bin icon removes the associated item.
+ * Handle Deletes and Edits:
+ * - clicking the bin icon removes the associated item.
+ * - clicking the edit icon allows user to edit the associated item.
  */
 list.addEventListener("click", (e) => {
-  if (e.target.matches("svg") || e.target.closest("span")) {
+  const bin = e.target.closest(".bin-icon")
+  const edit = e.target.closest(".edit-icon")
+
+  if (bin) {
     toDo.splice(e.target.closest("li").id, 1);
     refreshList();
-    console.log("Item deleted");
+  } else if (edit) {
+    // RUN THIS CODE WHEN USER PRESSES EDIT
+    // e.target.closest("li")
   }
 });
+
+/**
+ * TO-DO:
+ * 1. Implement edit feature
+ * 2. Improvee user input validation
+ * 3. Maybe store list in localStorage to ensure data is not lost
+ */
